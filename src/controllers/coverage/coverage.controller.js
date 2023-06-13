80a0d5572baa940dd3211e5636b98f4d3cfa8a57
Inamullah <inamullah@Inams-MBP.local>
2023-06-14 01:20:20 +0500

    coverage included in the gitignore

---
+++ src/controllers/coverage/coverage.controller.js
@@ -0,0 +1,83 @@
+// Import the Coverage model
+const { Coverage } = require('../../models');
+
+// Get all coverages
+exports.getAllCoverages = async (req, res) => {
+  try {
+    const coverages = await Coverage.findAll();
+    res.status(200).json(coverages);
+  } catch (error) {
+    console.error('Error retrieving coverages:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+// Get Coverage by Patient ID
+exports.getCoverageByPatientId = async (req, res) => {
+    try {
+      const { patientId } = req.params;
+      const coverage = await Coverage.findAll({ where: { patientId } });
+      res.json(coverage);
+    } catch (error) {
+      console.error('Error retrieving Coverage:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+// Get coverage by ID
+exports.getCoverageById = async (req, res) => {
+  const { coverageId } = req.params;
+  try {
+    const coverage = await Coverage.findByPk(coverageId);
+    if (!coverage) {
+      return res.status(404).json({ message: 'Coverage not found' });
+    }
+    res.status(200).json(coverage);
+  } catch (error) {
+    console.error('Error retrieving coverage:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+// Create a new coverage
+exports.createCoverage = async (req, res) => {
+  const { patientId, insuranceId, status, name } = req.body;
+  try {
+    const coverage = await Coverage.create({ patientId, insuranceId, status, name });
+    res.status(201).json(coverage);
+  } catch (error) {
+    console.error('Error creating coverage:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+// Update an existing coverage
+exports.updateCoverage = async (req, res) => {
+  const { coverageId } = req.params;
+  const { patientId, insuranceId, status, name } = req.body;
+  try {
+    const coverage = await Coverage.findByPk(coverageId);
+    if (!coverage) {
+      return res.status(404).json({ message: 'Coverage not found' });
+    }
+    await coverage.update({ patientId, insuranceId, status, name });
+    res.status(200).json(coverage);
+  } catch (error) {
+    console.error('Error updating coverage:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+// Delete a coverage
+exports.deleteCoverage = async (req, res) => {
+  const { patientId } = req.params;
+  try {
+    const coverage = await Coverage.findByPk(patientId);
+    if (!coverage) {
+      return res.status(404).json({ message: 'Coverage not found' });
+    }
+    await coverage.destroy();
+    res.status(204).end();
+  } catch (error) {
+    console.error('Error deleting coverage:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
